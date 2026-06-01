edaf610476de1a565b317bd82d5922c98a334609
YashuYadav001 <yashu3801@gmail.com>
2026-06-01 22:26:07 +0530

    implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import React from 'react'
-import { navLinks } from './constants'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="/logo.svg" alt="Apple logo" />
-          <ul>
-            {navLinks.map(({label}) => (<li key={label}>
-              <a href={label}>{label}</a>
-              </li>
-
-            ))}
-          </ul>
-
-          <div className='flex-center gap-3'>
-            <button>
-              <img src="/search.svg" alt="search" />
-            </button>
-            <button>
-              <img src="/cart.svg" alt="cart" />
-            </button>
-          </div>
-        </nav>
-      </header>
+      <Navbar />
+      <Hero />
       
     </div>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef } from 'react'
+
+
+
+const Hero = () => {
+  return (
+   <section id='hero'>
+     <div>
+        <h1>MacBook Pro</h1>
+        <img src="/title.png" alt="MacBook Title" />
+      
+    </div>
+    <video  src="/videos/hero.mp4" autoPlay muted playsInline></video>
+    <button>Buy</button>
+    <p>From $1599 or $133/mo for 12 months</p>
+   </section>
+  )
+}
+
+export default Hero
